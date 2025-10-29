c3cf0ee462bd28fdb2cca711dd6292f72c5455b9
Farhan00755 <tyoprasetyo2007@gmail.com>
2025-10-29 09:19:45 +0700

    revisi

--- src/components/card-event.jsx
+++ src/components/card-event.jsx
@@ -69,19 +69,19 @@ export default function CardEvent() {
     ];
 
     return (
-    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
-        {cardEvent.map((event) => (
-            <div key={event.idNumber} className="bg-white rounded-lg shadow-md overflow-hidden hover:translate-y-[-10px] hover:shadow-lg transition-all duration-300 mx-5 my-5">
-                <img src={event.image} alt={event.name} className="w-full h-48 object-cover" />
-                <div className="p-4">
-                    <h3 className="text-xl font-bold mb-2">{event.name}</h3>
-                    <p className="text-gray-600 mb-2">{event.day}, {event.date} at {event.time}</p>
-                    <p className="text-gray-600 mb-2">📍 {event.location}</p>
-                    <p className="text-gray-700">{event.description}</p>
-                    <p className="text-gray-800 text-xl font-bold mt-3">status : {event.status}</p>
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 p-6">
+            {cardEvent.map((event) => (
+                <div key={event.idNumber} className="bg-white rounded-lg shadow-md overflow-hidden hover:translate-y-[-10px] hover:shadow-lg transition-all duration-300 mx-5 my-5">
+                    <img src={event.image} alt={event.name} className="w-full h-48 object-cover" />
+                    <div className="p-4">
+                        <h3 className="text-xl font-bold mb-2">{event.name}</h3>
+                        <p className="text-gray-600 mb-2">{event.day}, {event.date} at {event.time}</p>
+                        <p className="text-gray-600 mb-2">📍 {event.location}</p>
+                        <p className="text-gray-700">{event.description}</p>
+                        <p className="text-gray-800 text-xl font-bold mt-3">status : {event.status}</p>
+                    </div>
                 </div>
-            </div>
-        ))}
-    </div>
-)
+            ))}
+        </div>
+    )
 }
